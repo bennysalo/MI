260f09fa0adc33a08b425f4fe18eab01e8f0aeb2
bennysalo <benny.salo@abo.fi>
2017-03-24 10:42:52 +0100

    Tidy up

--- 2a-anchors-functionsB.R
+++ 2a-anchors-functionsB.R
@@ -221,15 +221,21 @@ analyses_step_2 <- function(base_model, used_data, grouping, item_vector) {
   #   ... would also show as low chisq.diff when compared to strong model
   # hence that is the best referent item
   # This in turn also means small difference in chisq between partial and strong invariance models
-  results[["partial invariance table"]] <- compare_partials_to_strong(partial_models_list = results[["partial fits"]], 
-                                                                      strong_model = results[["strong fit"]])
+  results[["partial invariance table"]]         <- 
+    compare_partials_to_strong(partial_models_list = results[["partial fits"]], 
+                               strong_model = results[["strong fit"]])
   # 4. Assign referent items
-  results[["referent items"]] <- get_referent_items(fit_table = results[["partial invariance table"]])
-  results[["significance test referent items"]] <- subset(results[["partial invariance table"]],
-                                                          item %in% results[["referent items"]])
-  results[["configural fit"]] <- run_configural_model(base_model, used_data, grouping, results[["referent items"]])
-  results[["test strong invariance"]] <-lavTestLRT(results[["configural fit"]], results[["strong fit"]], 
-                                                   method = "satorra.bentler.2010") 
+  results[["referent items"]]                   <- 
+    get_referent_items(fit_table = results[["partial invariance table"]])
+  # 5. Pick out the referent items to see if they meet LRT test for invariance
+  results[["significance test referent items"]] <- 
+    subset(results[["partial invariance table"]],item %in% results[["referent items"]])
+  # 6. Fit configural model
+  results[["configural fit"]]                   <- 
+    run_configural_model(base_model, used_data, grouping, results[["referent items"]])
+  # 7. TEst invariance by comparing configural and strong invariance models
+  results[["test strong invariance"]]           <-
+    lavTestLRT(results[["configural fit"]], results[["strong fit"]], method = "satorra.bentler.2010") 
   return(results)
 }
 
