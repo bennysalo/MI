f0c9bccb24ded5455580d3e67ef2ccbe3cbbc6a8
bennysalo <benny.salo@abo.fi>
2017-05-22 18:12:29 +0200

    New analyses

---
+++ 22b-anchors-analyses-2.R
@@ -0,0 +1,90 @@
+# Load workspace with functions
+
+load("C:/Users/benny_000/Dropbox/to aws/MI-0-all functions and data.R.RData")
+
+
+
+
+# Define preset objects that will be used in all five groupings
+
+
+model_for_all <- Mod6facMI2
+
+# create a vector of item names for items that only load on one factor
+unidim_items <- subset(lavaanify(model_for_all), op == "=~")$rhs
+# Pick items that only occur once in the parameter table
+unidim_items <- names(table(unidim_items))[table(unidim_items) == 1]
+
+data_for_all <- FinPrisonMales2
+
+save.image("~/Dropbox/to aws/MI workspace after 1a.RData")
+
+
+
+# Median split on AGE
+print(Sys.time())
+start<-Sys.time()
+results.age.2 <- analyses_step_2(base_model = model_for_all, 
+                                     used_data  = data_for_all, 
+                                     grouping   = "ageMedSplit", 
+                                     item_vector = unidim_items)
+
+Sys.time()-start
+
+save.image("~/Dropbox/to aws/MI age 2.RData")
+
+
+# ### VIOLENCE
+print(Sys.time())
+start<-Sys.time()
+results.violence.2 <- analyses_step_2(base_model = model_for_all, 
+                                          used_data  = data_for_all, 
+                                          grouping   = "violentCrime", 
+                                          item_vector = unidim_items)
+
+Sys.time()-start
+
+save.image("~/Dropbox/to aws/MI violence 2.RData")
+
+
+# ### PREVIOUS
+
+print(Sys.time())
+start<-Sys.time()
+results.previous.2 <- analyses_step_2(base_model = model_for_all, 
+                                          used_data  = data_for_all, 
+                                          grouping   = "prevReoffence", 
+                                          item_vector = unidim_items)
+
+Sys.time()-start
+
+save.image("~/Dropbox/to aws/MI previous 2.RData")
+
+
+# ### REOFFENCE
+
+print(Sys.time())
+start<-Sys.time()
+results.reoffence.2 <- analyses_step_2(base_model = model_for_all, 
+                                            used_data  = data_for_all, 
+                                            grouping   = "reoffender", 
+                                            item_vector = unidim_items)
+
+Sys.time()-start
+
+save.image("~/Dropbox/to aws/MI reoffence 2.RData")
+
+
+# ### CLOSED
+print(Sys.time())
+start<-Sys.time()
+results.closed.2 <- analyses_step_2(base_model = model_for_all, 
+                                        used_data  = data_for_all, 
+                                        grouping   = "allClosed", 
+                                        item_vector = unidim_items)
+
+Sys.time()-start
+
+save.image("~/Dropbox/to aws/MI closed 2.RData")
+
+
