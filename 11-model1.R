60e0ba4959cdd71621c28861f351f6034291f6b6
bennysalo <benny.salo@abo.fi>
2017-05-23 11:51:20 +0200

    Added back 'Mod6facMI2'

--- 11-model1.R
+++ 11-model1.R
@@ -42,3 +42,23 @@ ls()
 # Save results so far
 save.image("~/Dropbox/to aws/MI workspace after 1a.RData")
 
+# i_instrumentalAggression delted from change and i_remedialTeaching delted from employment
+Mod6facMI2 <-' 
+economy    =~ i_financialManagement + i_financialObstacles + i_financialSituation + 
+              i_dailyLifeManagement + ic_accomodation +  i_workApplication
+alcohol    =~ i_alcFrequency + i_alcEffectWork + i_alcEffectRelations + i_alcEffectHealth +
+              i_alcWithMeds + i_alcMotivationTreat + i_alcViolence
+change     =~ i_motivationChange + i_attitudeStaff + i_attitudeSupervision + i_othersView + i_attitudeHostile +
+              i_insight + i_manipulative + i_attitudeProcrime + i_socialSkills + i_alcMotivationTreat + i_workAttitude
+drugs      =~ i_peersCriminal + i_riskSeeking + i_drugHistory +ifact_drugUse +
+              i_alcWithMeds + i _attitudeProcrime
+aggression =~ i_impulsive + i_alcViolence + i_domViolPerp + i_instrumentalAggression + i_attitudeHostile +
+              i_othersView + i_attitudeStaff
+employment =~ i_workHistory + i_eduNeed + i_eduAttitude + i_workAttitude + i_workApplication'
+           
+
+CFA6facMI2 <- cfa(Mod6facMI2, data = FinPrisonMales2, std.lv = T, estimator = "WLSMV")
+summary(CFA6facMI2, fit = T)
+head(modindices(CFA6facMI2, sort. = TRUE))
+
+
