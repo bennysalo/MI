d3b569e2e364d3ad0a3f7dbda3d935f0efe7d454
bennysalo <benny.salo@abo.fi>
2017-05-24 11:35:04 +0200

    Quicker setup

--- 00-setup.R
+++ 00-setup.R
@@ -1,17 +1,28 @@
 # Setup access to dropbox in AWS EC2
-install.packages(c("lavaan", "semTools", "simsem", "boot", "tidyverse", "psych", "SparseM", "car"))
-
 library(RStudioAMI)
-linkDropbox()
 excludeSyncDropbox("*")
 includeSyncDropbox("to aws")
 
-library(purrr)
-library(dplyr)
+
+# Short setup
+install.packages(c("lavaan", "semTools", "simsem", "purrr"))
 library(lavaan)
+library(simsem)                 
 library(semTools)
-library(psych)
-library(boot)
-library(simsem)
-library(ggplot2)
-library(car)
+library(purrr)
+
+
+
+# # Full setup
+# install.packages(c("lavaan", "semTools", "simsem", "boot", "tidyverse", "psych", "SparseM", "car"))
+# 
+# 
+# library(purrr)
+# library(dplyr)
+# library(lavaan)
+# library(semTools)
+# library(psych)
+# library(boot)
+# library(simsem)   
+# library(ggplot2)
+# library(car)
